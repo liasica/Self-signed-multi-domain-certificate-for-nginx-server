e5a9202f6e9c279cd1d9324fb809db04a4427721
liasica <magicrolan@qq.com>
2017-02-20 12:50:15 +0800

    post files to github.com

--- ssl.sh
+++ ssl.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 #
 # thx for http://www.cnblogs.com/liqingjht/p/6267563.html
+mkdir cert
 echo "Generating an SSL private key to sign your certificate..."
-openssl genrsa -des3 -out server.key 2048
+openssl genrsa -des3 -out cert/server.key 2048
 
 echo "Generating a Certificate Signing Request..."
-openssl req -new -key server.key -out server.csr -config ./openssl.cnf
+openssl req -new -key cert/server.key -out cert/server.csr -config ./openssl.cnf
 
 echo "Removing passphrase from key (for nginx)..."
-cp server.key server.key.org
-openssl rsa -in server.key.org -out server.key
-rm server.key.org
+cp cert/server.key cert/server.key.org
+openssl rsa -in cert/server.key.org -out cert/server.key
+rm cert/server.key.org
 
 echo "Generating CA..."
-mkdir ./newcerts
 touch index.txt
 echo 01 > serial
-openssl req -new -x509 -days 3650 -keyout ca.key -out ca.crt -config ./openssl.cnf
+openssl req -new -x509 -days 3650 -keyout ca.key -out ca.crt -config openssl.cnf
 
 echo "Generating certificate..."
-openssl ca -in server.csr -out server.crt -cert ca.crt -keyfile ca.key -extensions v3_req -config openssl.cnf
+openssl ca -in cert/server.csr -out cert/server.crt -cert ca.crt -keyfile ca.key -extensions v3_req -config openssl.cnf
 
-echo "Copying certificate (server.crt) to /usr/local/etc/nginx/ssl/"
+echo "Copying certificate (cert/server.crt) to /usr/local/etc/nginx/ssl/"
 mkdir -p /usr/local/etc/nginx/ssl/
-rm -rf /usr/local/etc/nginx/ssl/server.crt
-cp -f server.crt /usr/local/etc/nginx/ssl/
+rm -rf /usr/local/etc/nginx/ssl/cert/server.crt
+cp -f cert/server.crt /usr/local/etc/nginx/ssl/
 
-echo "Copying key (server.key) to /usr/local/etc/nginx/ssl/"
+echo "Copying key (cert/server.key) to /usr/local/etc/nginx/ssl/"
 mkdir -p /usr/local/etc/nginx/ssl/
-rm -rf /usr/local/etc/nginx/ssl/server.key
-cp server.key /usr/local/etc/nginx/ssl/
+rm -rf /usr/local/etc/nginx/ssl/cert/server.key
+cp cert/server.key /usr/local/etc/nginx/ssl/
